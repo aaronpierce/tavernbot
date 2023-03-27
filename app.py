9c1c4550668ae9056a31567d20d70c525ddf0e9d
root <root@do.ubuntu>
2023-03-27 16:55:56 +0000

    app.py update

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import os
 import random
 import discord
+from discord.ext.commands import Bot
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,60 +11,16 @@ GUILD = os.getenv('DISCORD_GUILD')
 OWNER = os.getenv('DISCORD_OWNER')
 PREFIX = os.getenv('DISCORD_PREFIX')
 
-client = discord.Client(guild_subscriptions=True, intents=discord.Intents.default())
+intents = discord.Intents.all()
 
+bot = Bot(command_prefix=PREFIX, intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user.name} has connected to Discord!')
-    for member in client.get_all_members():
-        print(member)
+    print('Tavern Bot Connected.')
 
+@bot.command():
+async def hello(ctx):
+        await ctx.channel.send(f'hello there {ctx.author.mention}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    one = ['Hello.',
-           'Hi',
-           'Hey!',
-           'Hi there!',
-           'Hey there!',
-           'Hey man!',
-           'Hey bro!',
-           'Hey dude!',
-           'Hey buddy!',
-           'Yo!',
-           'Howdy!']
-
-    two = ['How are you?',
-           'How are ya?',
-           'How are things?',
-           'How are things going?',
-           'How’s it going?',
-           'What’s going on?',
-           'How have you been?',
-           'How’s everyone?',
-           'How’s everyone doing?  ',
-           'Whazzup?',
-           'Sup?  ',
-           'What’s happening?',
-           'What are you up to these days?',
-           'What’s new?',
-           'What’s shaking?',
-           'What’s shakin’?',
-           'How are you holding up?',
-           'Doing OK?',
-           'Everything OK?']
-
-    if message.content == '!Hi':
-        response = random.choice(one) + ' ' + random.choice(two)
-        await message.channel.send(response)
-
-    if message.content == '!m':
-        response = 'm!help' # https://open.spotify.com/artist/6159IBm5gLPwG4BcJXseXc?si=2Hc9KEl2Q82l3bXp3Coo_A'
-        await message.channel.send(response)
-
-
-client.run(TOKEN)
+bot.run(TOKEN)
