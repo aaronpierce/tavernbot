c6718ab8f540da5c03173ef743ef8dc3d97f5630
Aaron Pierce <aaronpierce15@gmail.com>
2023-04-08 00:40:21 -0400

    Added tavern rpg connection

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from bot import TavernBot, BotConfig
+from TavernRPG.server import RPG
+from TavernRPG.client import Client
 
 config = BotConfig()
-tavernbot = TavernBot(config.template)
+template = config.template
+template['rpg'] = RPG
+template['client'] = Client
 
+tavernbot = TavernBot(template)
 tavernbot.run(config.token)
--- bot/bot.py
+++ bot/bot.py
@@ -1,10 +1,19 @@
 from discord.ext.commands import Bot
+import threading
 
 class TavernBot(Bot):
     def __init__(self, template):
         super().__init__(**template)
 
+        self.rpg = template['rpg']()
+        self.client = template['client']
+    
+        self.server_thread = threading.Thread(target=self.rpg.start)
+        self.server_thread.daemon = False
+        self.server_thread.start()
+
         self.add_commands(self)
+
 
     async def on_ready(self):
         print('TavernBot hast cometh to the v\'rmilion minotaur.')
@@ -22,3 +31,7 @@ class TavernBot(Bot):
         @bot.command()
         async def hello(ctx):
             await ctx.channel.send(f'Greetings to thee {ctx.author.mention}... nice tuh see ya.')
+
+        @bot.command()
+        async def play(ctx):
+            await self.client('localhost', 9999)
